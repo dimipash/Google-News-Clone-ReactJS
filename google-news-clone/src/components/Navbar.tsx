d124decf30a74443374850bffa7b16db10fb61fd
Dimitar Pashev <dim.pashev@gmail.com>
2024-04-05 18:54:49 +0300

    initial commit

---
+++ google-news-clone/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import google from "../assets/google.png";
+import lens from "../assets/lens.png";
+
+const Navbar = () => {
+	return (
+        <div className="flex items-center">
+		<div className="ml-5">
+			<img src={google} className="w-30 h-20" alt="Google News" />
+		</div>
+        <div className="ml-32 bg-zinc-100 flex items-center p-3 w-6/12 rounded-lg">
+            <img src={lens} className="w-5 h-5" alt="Search" />
+            <input type="text" placeholder="Search for news" className="bg-zinc-100 ml-4" />
+        </div>
+        <button className="ml-44 bg-blue-600 text-white p-2 w-28 rounded-md">Sign in</button>
+        </div>        
+	);
+};
+
+export default Navbar;
